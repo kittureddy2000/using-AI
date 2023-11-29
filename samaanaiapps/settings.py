cef439c95745ce0aa6d8f88a5e8f8acf71c32356
kittureddy2000 <kittureddy2000@gmail.com>
2023-11-28 18:00:25 -0800

    sdf

--- samaanaiapps/settings.py
+++ samaanaiapps/settings.py
@@ -65,7 +65,9 @@ elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     print("Inside Google Cloud Project Environment")
 
     client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settingswerwer")
+    print("Settings name ")
+    print(settings_name )
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
