eeeeae676bbb54ecf51b89b7e3d88bef6155ccd6
kittureddy2000 <kittureddy2000@gmail.com>
2023-11-29 14:19:53 -0800

    sdf

--- core/utils.py
+++ core/utils.py
@@ -6,7 +6,7 @@ def access_secret(project_id, secret_id, version_id="latest"):
     Access a single secret version in Google Secret Manager.
     """
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/1"#{version_id}"
+    name = f"projects/{project_id}/secrets/{secret_id}/{version_id}"
     print(name)
     response = client.access_secret_version(request={"name": name})
     return response.payload.data.decode("UTF-8")
