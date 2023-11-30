52fe635de44018c756d3b52700d9f33a52915916
kittureddy2000 <kittureddy2000@gmail.com>
2023-11-30 14:05:24 -0800

    sdf

--- todos/models.py
+++ todos/models.py
@@ -5,6 +5,8 @@ class Task(models.Model):
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.title
