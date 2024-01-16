24c23e823c8a6e2227af7a9fa296eff95785f604
kittureddy2000 <kittureddy2000@gmail.com>
2024-01-15 17:25:58 -0800

    jj

--- samaanaiapps/settings.py
+++ samaanaiapps/settings.py
@@ -184,7 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #STATIC_URL = 'https://storage.googleapis.com/using-ai-samaan/static/'
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
 
