273112de0015cc4baacc9d6710237dceb71a53d3
“kittureddy2000@gmail.com” <kittureddy2000@gmail.com>
2024-08-31 15:14:58 -0700

    Fix for SP Error

--- spreturn/views.py
+++ spreturn/views.py
@@ -58,6 +58,9 @@ def spreturn(request):
     # Creating dictionaries from the queryset
     SP500 = {data.year: data.spreturn for data in spreturn_data}
     SP500_dividend = {data.year: data.return_divident for data in spreturn_data}
+    print("SP500 Data :" + SP500)
+    print("SP500 Dividend Data :" + SP500_dividend)
+    
 
 
     for start_year  in range(1928, 2021 - number_of_year):
