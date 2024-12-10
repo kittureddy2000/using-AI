20f88a2cbb7fec1c2e6358aa6748f20aa11e0d94
kittureddy2000 <kittureddy2000@gmail.com>
2024-12-09 21:56:52 -0800

    fixed the defaul

--- core/urls.py
+++ core/urls.py
@@ -9,8 +9,8 @@ app_name = 'core'  # Define the namespace
 
 urlpatterns = [
 
+    path('', views.dashboard, name='dashboard'),  # Root URL to render the dashboard view
     path('accounts/signup/', CustomSignupView.as_view(), name='account_signup'),
-
     path('accounts/', include('allauth.urls')),  # Social login routes
     path('dashboard/', views.dashboard, name='dashboard'),  # Named 'dashboard'
     path('profile/', views.profile, name='profile'),
